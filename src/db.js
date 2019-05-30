b1a5cdfdd6cc5a726007a984b900f2bcb8858637
Yurii <dyakivyurii@gmail.com>
2019-05-30 18:19:22 +0300

    [fix, feature] fixed small error with modifing recipe, search recipes

--- src/db.js
+++ src/db.js
@@ -5,8 +5,8 @@ const db = knex({
   client: 'pg',
   connection: {
     host: '127.0.0.1',
-    user: 'postgres',
-    password: 'root',
+    user: '',
+    password: '',
     database: 'cookbook'
   }
 });
